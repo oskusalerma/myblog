95e957b292d847dbad8d33fd624d55c6ab3a0870
Osku Salerma <osku.salerma@gmail.com>
2016-02-03 14:02:58 +0000

    Improve Post admin.

--- myblog/main/admin.py
+++ myblog/main/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from .models import BlogInfo, Post
 
+class PostAdmin(admin.ModelAdmin):
+    fields = (("author", "title"), ("id", "pub_date"), "content")
+    readonly_fields = ("pub_date", "id")
+
+    list_display = ("title", "author", "pub_date", "id")
+    list_display_links = ("title",)
+
 admin.site.register(BlogInfo)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
