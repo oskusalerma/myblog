a38395537ae5bc59b50851aa58dc568baf062ce0
Osku Salerma <osku.salerma@gmail.com>
2016-02-04 11:56:34 +0000

    Switch from sqlite to postgresql.

--- initial-setup.sh
+++ initial-setup.sh
@@ -4,3 +4,4 @@ virtualenv venv
 . venv/bin/activate
 pip install -U pip
 pip install Django
+pip install psycopg2
--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "myblog",
+        "USER": os.environ["USER"],
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
     }
 }
 
